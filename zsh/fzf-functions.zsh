febc735e4d7444d6725f6b3d3e8bbc5bf00fbbfb
Earl <k2sy12@inf.elte.hu>
2020-05-20 12:14:34 +0200

    Autocommit.

--- zsh/fzf-functions.zsh
+++ zsh/fzf-functions.zsh
@@ -5,23 +5,23 @@ fa() {
   cd "$dir"
 }
 
-fkil() {
-  local pid
-  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+# fkil() {
+#   local pid
+#   pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
 
-  if [ "x$pid" != "x" ]
-  then
-    echo $pid | xargs kill -${1:-9}
-  fi
+#   if [ "x$pid" != "x" ]
+#   then
+#     echo $pid | xargs kill -${1:-9}
+#   fi
+# }
+
+ fg() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
 }
-
-# fg() {
-#  git log --graph --color=always \
-#      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-#  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-#      --bind "ctrl-m:execute:
-#                (grep -o '[a-f0-9]\{7\}' | head -1 |
-#                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-#                {}
-#FZF-EOF"
-#}
