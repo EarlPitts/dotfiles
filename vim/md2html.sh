0aeebc12c4a0f6b518bb371866697ab64e1590cf
Bendeguz Seres <grafitpor@gmail.com>
2020-10-23 09:23:53 +0200

    Autocommit.

--- vim/md2html.sh
+++ vim/md2html.sh
@@ -15,8 +15,12 @@ FORCEFLAG=
 [ $SYNTAX = "markdown" ] || { echo "Error: Unsupported syntax"; exit -2; };
 
 OUTPUT="$OUTPUTDIR"/$(basename "$INPUT" .$EXTENSION).html
+MATHJAX1='<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>'
+MATHJAX2='<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>'
+INLINE='<script>window.MathJax={tex:{inlineMath:[["$","$"],["\\(","\\)"]]}};</script>'
 
-$MKD2HTML -css "style.css" "$INPUT"
+
+$MKD2HTML -css "style.css" -header "$MATHJAX1" -header "$MATHJAX2" -header "$INLINE" "$INPUT"
 OUTPUTTMP=$(dirname "$INPUT")/$(basename "$INPUT" ."$EXTENSION").html
 mv -f "$OUTPUTTMP" "$OUTPUT"
 
