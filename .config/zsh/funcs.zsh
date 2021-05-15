b00ea8699adaf69f74956c6ed069b0327c252029
Bendeguz Seres <grafitpor@gmail.com>
2021-05-15 07:31:13 +0200

    Move functions from fzf-funcs to funcs.

--- .config/zsh/funcs.zsh
+++ .config/zsh/funcs.zsh
@@ -97,6 +97,34 @@ gitgrep() {
     git grep "$1" $(git rev-list --all)
 }
 
+fa() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+# fkil() {
+#   local pid
+#   pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+
+#   if [ "x$pid" != "x" ]
+#   then
+#     echo $pid | xargs kill -${1:-9}
+#   fi
+# }
+
+fg() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
+
 # # Delete branches that have been squashed and merged into master (https://github.com/not-an-aardvark/git-delete-squashed)
 # # TODO: git-trim may replace this
 # gdelsquashed() {
