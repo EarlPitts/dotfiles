f0d70f2d2d71e4aefaeac82a6fc7386a5438505b
Bendeguz Seres <grafitpor@gmail.com>
2021-05-25 07:19:48 +0200

    Zsh backspace fix.

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -1,8 +1,9 @@
 #!/usr/bin/zsh
 
-bindkey -v # vim keybindings
+bindkey -v                                      # Vim keybindings
 
 bindkey '^ ' autosuggest-accept
+ bindkey -v '^?' backward-delete-char           # Backspace stuck in vim mode
 
 bindkey -s '^u' 'references\n'
 bindkey -s '^o' 'productivity\n'
