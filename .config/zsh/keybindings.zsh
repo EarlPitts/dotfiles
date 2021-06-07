ea03d80004d03f9cbc70e4c3c3afdb25606695e8
Bendeguz Seres <grafitpor@gmail.com>
2021-06-07 18:31:32 +0200

    Turn off zsh widgets for now.

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -5,6 +5,6 @@ bindkey -v                                      # Vim keybindings
 bindkey '^ ' autosuggest-accept
 bindkey -v '^?' backward-delete-char            # Backspace stuck in vim mode
 
-bindkey -s '^u' 'references^M'
-bindkey -s '^o' 'productivity^M'
+# bindkey -s '^u' 'references^M'
+# bindkey -s '^o' 'productivity^M'
 #bindkey -s '^i' 'quick_note open^M'
