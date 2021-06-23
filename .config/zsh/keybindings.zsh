bd98ca87fc283fd4311a79117c4b720bbf4a4d46
Bendeguz Seres <grafitpor@gmail.com>
2021-06-23 08:13:38 +0200

    Add ^Z keybinding for fg.

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -5,6 +5,12 @@ bindkey -v                                      # Vim keybindings
 bindkey '^ ' autosuggest-accept
 bindkey -v '^?' backward-delete-char            # Backspace stuck in vim mode
 
+# Pressing ^Z a second time will bring back
+# the process from the background
+_zsh_cli_fg() { fg; }
+zle -N _zsh_cli_fg
+bindkey '^Z' _zsh_cli_fg
+
 # bindkey -s '^u' 'references^M'
 # bindkey -s '^o' 'productivity^M'
 #bindkey -s '^i' 'quick_note open^M'
