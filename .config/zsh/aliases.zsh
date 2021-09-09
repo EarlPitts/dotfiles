48bbee0e370b06da46e692647cea625acf86bff9
Bendeguz Seres <grafitpor@gmail.com>
2021-09-09 10:18:35 +0200

    Windows lockscreen prank

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias clean='cat ~/.local/share/bleach.txt | xargs bleachbit -c && yay -Sc --cle
 alias ls='exa -l'
 alias scan='sudo iw dev wlp4s0 scan | egrep "signal|SSID" | sed -e "s/\tsigna    l: //" -e "s/\tSSID: //" | awk '\''{ORS = (NR % 2 == 0)? "\n" : " "; print}    '\'' | sort'
 alias weather='curl wttr.in/Budapest'
-alias win='i3lock -p win -u -i ~/.dotfiles/wallpapers/winLock.jpg'
 alias cat='bat'
 alias drill='dog'
 alias dig='dog'
