b36f8ab279a094cfe4ae63c4a257c1308bfab382
Bendeguz Seres <grafitpor@gmail.com>
2022-05-18 07:55:16 +0200

    Clean up neovim keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,65 +1,61 @@
 vim.g.mapleader = ","
-vim.g.maplocalleader = ","                  --Needed for conjure
+vim.g.maplocalleader = ","                                  -- Needed for conjure
 vim.opt.pastetoggle = '<leader>p'
 
-local map = vim.api.nvim_set_keymap
-local noremap = {noremap = true}
+local map = vim.keymap.set
 
-map('n', ';', ':', noremap)
-map('n', '<leader>,', ':w<CR>', noremap)
-map('n', '<leader>.', ':Tagbar<CR>', noremap)
-map('n', '<F2>', ':UndotreeToggle<CR>', noremap)
-map('n', '<F12>', ':tabnew wiki/index.md<CR>:Tagbar<CR>', noremap)
-map('n', '<leader>j', ':%!python -m json.tool<CR>', noremap) --Format JSON
-map('', '<F1>', ':!./%<CR>', noremap)
-map('n', '<leader>md', ':MarkdownPreview<CR>', noremap)
-map('n', '<leader>sr', ':ConjureEval (RESTART 1)<CR>', noremap) -- Restart Scheme
+map('n', ';', ':')
+map('n', '<leader>,', ':w<CR>')
+map('n', '<leader>.', ':Tagbar<CR>')
+map('n', '<F2>', ':UndotreeToggle<CR>')
+map('n', '<F12>', ':tabnew wiki/index.md<CR>:Tagbar<CR>')
+map('n', '<leader>j', ':%!python -m json.tool<CR>')         -- Format JSON
+map('', '<F1>', ':!./%<CR>')
+map('n', '<leader>md', ':MarkdownPreview<CR>')
+map('n', '<leader>sr', ':ConjureEval (RESTART 1)<CR>')      -- Restart Scheme
 
---Make
-map('', '<F5>', ':make!<CR>', noremap)
---map('', '<F5>', ':!~/.scripts/compile.sh %:p<CR>', noremap)
-map('', '<F6>', ':make test<CR>', noremap)
+-- Make
+map('', '<F5>', ':make!<CR>')
+-- map('', '<F5>', ':!~/.scripts/compile.sh %:p<CR>')
+map('', '<F6>', ':make test<CR>')
 
---Version Control
-map('n', '<F10>', ':Gdiffsplit<CR>', noremap)
-map('n', '<leader>g', ':Git<CR>', noremap)
+-- Version Control
+map('n', '<F10>', ':Gdiffsplit<CR>')
+map('n', '<leader>g', ':Git<CR>')
 
---FZF
-map('n', '<C-b>', ':Buffers<CR>', noremap)
-map('n', '<C-g>', ':Rg<CR>', noremap)
-map('n', '<C-t>', ':Tags<CR>', noremap)
-map('n', '<C-s>', ':Files ~/References/Snippets<CR>', noremap)
---map('n', '<C-s>', ':Snippets<CR>', noremap)
-
-map('i', '<C-n>', '<C-x><C-o>', noremap )              --Remap omnicomplete
---map('n', '<Space>', 'za', noremap)                    --Folding
-
---Copy to clipboard
-map('n', '<C-c>', '"*yy', noremap)
-map('v', '<C-c>', '"*y', noremap)
-
---Window switching
-map('t', '<C-h>', '<C-\\><C-n><C-w>h', noremap)
-map('t', '<C-j>', '<C-\\><C-n><C-w>j', noremap)
-map('t', '<C-k>', '<C-\\><C-n><C-w>k', noremap)
-map('t', '<C-l>', '<C-\\><C-n><C-w>l', noremap)
-map('i', '<C-h>', '<C-\\><C-n><C-w>h', noremap)
-map('i', '<C-j>', '<C-\\><C-n><C-w>j', noremap)
-map('i', '<C-k>', '<C-\\><C-n><C-w>k', noremap)
-map('i', '<C-l>', '<C-\\><C-n><C-w>l', noremap)
-map('n', '<C-h>', '<C-w>h', noremap)
-map('n', '<C-j>', '<C-w>j', noremap)
-map('n', '<C-k>', '<C-w>k', noremap)
-map('n', '<C-l>', '<C-w>l', noremap)
-map('n', '<C-w>-', '5<C-w>-', noremap)
-map('n', '<C-w>+', '5<C-w>+', noremap)
-map('n', '<C-w>,', '10<C-w><', noremap)
-map('n', '<C-w>.', '10<C-w>>', noremap)
-
+-- Telescope
+map('n', '<C-b>', ':Telescope buffers<CR>')
+map('n', '<C-g>', ':Telescope live_grep<CR>')
+map('n', '<C-t>', ':Telescope tags<CR>')
+map('n', '<C-s>', ':lua require"telescope.builtin".find_files({cwd = "~/References/Snippets"})<CR>')
+-- git awareness for file picker
 if vim.fn.isdirectory(".git") == 1 then
-	-- if in a git project, use :GFiles
-    map('n', '<C-p>', ':GitFiles<CR>', noremap)
+    map('n', '<C-p>', ':Telescope git_files<CR>')
 else
-	-- otherwise, use :FZF
-    map('n', '<C-p>', ':Files<CR>', noremap)
+    map('n', '<C-p>', ':Telescope find_files<CR>')
 end
+
+map('i', '<C-n>', '<C-x><C-o>' )                            -- Remap omnicomplete
+-- map('n', '<Space>', 'za')                                -- Folding
+
+-- Copy to clipboard
+map('n', '<C-c>', '"*yy')
+map('v', '<C-c>', '"*y')
+
+-- Window switching
+map('t', '<C-h>', '<C-\\><C-n><C-w>h')
+map('t', '<C-j>', '<C-\\><C-n><C-w>j')
+map('t', '<C-k>', '<C-\\><C-n><C-w>k')
+map('t', '<C-l>', '<C-\\><C-n><C-w>l')
+map('i', '<C-h>', '<C-\\><C-n><C-w>h')
+map('i', '<C-j>', '<C-\\><C-n><C-w>j')
+map('i', '<C-k>', '<C-\\><C-n><C-w>k')
+map('i', '<C-l>', '<C-\\><C-n><C-w>l')
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+map('n', '<C-w>-', '5<C-w>-')
+map('n', '<C-w>+', '5<C-w>+')
+map('n', '<C-w>,', '10<C-w><')
+map('n', '<C-w>.', '10<C-w>>')
