a84fceefe71bc423a588ffc68d1bfeff716504d0
Bendeguz Seres <grafitpor@gmail.com>
2022-07-02 16:09:47 +0200

    Neovim, haskell tabsize 2

---
+++ .config/nvim/ftplugin/haskell.lua
@@ -0,0 +1,5 @@
+local set = vim.opt
+
+set.shiftwidth = 2                          -- Indent size
+set.tabstop = 2                             -- How many columns a tab counts for
+set.softtabstop = 2                         -- Number of columns when you hit tab
