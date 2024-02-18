929093dd654b997049293eabeaa394aa5f9e064b
Bendeguz Seres <grafitpor@gmail.com>
2024-02-18 08:13:44 +0100

    Metals LSP config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -50,3 +50,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
   end,
 })
+
+
+local metals_config = require("metals").bare_config()
+metals_config.init_options.statusBarProvider = "on"
+
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "scala", "sbt", "java" },
+    callback = function()
+        require("metals").initialize_or_attach(metals_config)
+    end,
+    group = nvim_metals_group,
+})
+
+-- metals_config.on_attach = function(client, bufnr)
+--     require("metals").setup_dap()
+-- end
