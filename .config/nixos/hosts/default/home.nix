36fd047d003e49c5c9b072e5f70cba049d1a35de
Bendeguz Seres <grafitpor@gmail.com>
2024-05-18 13:14:52 +0200

    Organize packages

--- .config/nixos/hosts/default/home.nix
+++ .config/nixos/hosts/default/home.nix
@@ -15,13 +15,11 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
+  services.gpg-agent.pinentryPackage = pkgs.pinentry-rofi;
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -34,30 +32,36 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    # CLI tools
     tree
-    neovim
     eza
     bat
     ripgrep
     fd
-    tmux
-    git
-    firefox
-    rofi
     fzf
-    direnv
-    rxvt-unicode
-    xmobar
+    rofi
     nnn
-    gcc
+
+    # Devtools
+    neovim
+    git
+    direnv
+
+    # GUI
+    gnome.adwaita-icon-theme
     feh
     sxiv
+    unclutter-xfixes
+
+    tmux
+    firefox
+    gcc
     gnupg
     pinentry-rofi
     polkit
-    unclutter
     iw # i3blocks
     pulseaudio # For pactl (i3blocks)
+    udiskie
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
