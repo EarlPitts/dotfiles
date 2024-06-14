ff3e2411534dc16175bb3c45780ed2f76d55cf42
Bendeguz Seres <bendeguz.seres@emarsys.com>
2024-06-14 06:19:24 +0200

    Add nix config for darwin

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -4,18 +4,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/default/configuration.nix
-        inputs.home-manager.nixosModules.default
+        home-manager.nixosModules.default
+      ];
+    };
+    darwinConfigurations."FYPYG0PQFC" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+      ./hosts/darwin/configuration.nix
+       home-manager.darwinModules.home-manager
       ];
     };
   };
