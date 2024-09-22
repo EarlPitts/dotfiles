bd8af9f61e2e90a2cfa22c7b6de17aa9faffec0d
Bendeguz Seres <grafitpor@gmail.com>
2024-09-22 20:39:53 +0200

    set cpu governor on battery

--- .config/nixos/hosts/default/configuration.nix
+++ .config/nixos/hosts/default/configuration.nix
@@ -176,7 +176,7 @@
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
 
         CPU_ENERGY_PERF_POLICY_ON_AC = ""; # Needed for throttled
 
