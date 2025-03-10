e0c946b64e66096955311aac512414a48e683eef
Bendeguz Seres <grafitpor@gmail.com>
2025-03-10 06:27:54 +0100

    Switch back to TLP

--- .config/nixos/hosts/default/configuration.nix
+++ .config/nixos/hosts/default/configuration.nix
@@ -153,18 +153,21 @@
   };
 
   # services.thermald.enable = true;
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-    battery = {
-      governor = "performance";
-      turbo = "auto";
-      enable_thresholds = true;
-      start_threshold = 75;
-      stop_threshold = 80;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = ""; # Needed for throttled
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 40;
+
+      START_CHARGE_THRESH_BAT1 = 75;
+      STOP_CHARGE_THRESH_BAT1 = 80;
     };
   };
 
