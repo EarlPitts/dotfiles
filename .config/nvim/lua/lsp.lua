69df3625cdcdd94133e28b9e80725d1a9cad43ef
Bendeguz Seres <bendeguz.seres@emarsys.com>
2025-03-18 07:33:17 +0100

    Use eslint for formatting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -56,6 +56,7 @@ lspconfig.ts_ls.setup {
     "vue",
   },
 }
+lspconfig.eslint.setup {};
 
 lspconfig.purescriptls.setup {
   settings = {
@@ -103,7 +104,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
+      vim.lsp.buf.format { filter = function(client) return client.name ~= "volar" end }
     end, opts)
     vim.keymap.set("n", "<space>K", function()
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
