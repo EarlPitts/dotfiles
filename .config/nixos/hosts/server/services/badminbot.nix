69486853cd01ceaeef684f88f2bd92fb783100c2
Bendeguz Seres <grafitpor@gmail.com>
2026-05-21 17:44:50 +0200

    Sunset badminbot-v2

--- .config/nixos/hosts/server/services/badminbot.nix
+++ .config/nixos/hosts/server/services/badminbot.nix
@@ -1,20 +1,6 @@
 { pkgs, ... }:
 {
-  systemd.services.badminbotV2 = {
-    description = "Badminbot";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "/home/ben/Projects/badminbot-v2/venv/bin/python /home/ben/Projects/badminbot-v2/bot.py";
-      EnvironmentFile = "/home/ben/Projects/badminbot-v2/.env";
-      Restart = "on-failure";
-      WorkingDirectory = "/home/ben/Projects/badminbot-v2";
-      User = "ben";
-    };
-  };
-
-  systemd.services.badminbotV3 = {
+  systemd.services.badminbot = {
     description = "Badminbot";
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
