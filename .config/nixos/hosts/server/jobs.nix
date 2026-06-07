7825a216abc0242abb8936c2a0ecfd62856ae56f
Bendeguz Seres <grafitpor@gmail.com>
2026-06-07 12:08:55 +0200

    Use the inbuilt nix borg service for backups

--- .config/nixos/hosts/server/jobs.nix
+++ .config/nixos/hosts/server/jobs.nix
@@ -1,52 +1,39 @@
 { pkgs, ... }:
 {
-  # Daily backup
-  systemd.timers."backup" = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "*-*-* 2:00:00";
-      Unit = "backup.service";
+
+  # Backups
+  services.borgbackup.jobs = {
+    home = {
+      paths = [ "/home/ben" ];
+      repo = "/mnt/Backup/home";
+      startAt = "*-*-* 2:00:00";
+      encryption = {
+        mode = "repokey";
+        passCommand = "cat /root/.borg-pass";
+        # passCommand = "${pkgs.pass}/bin/pass Misc/ServerBackup"; TODO
+      };
+      prune.keep = {
+        daily = 7;
+        weekly = 4;
+      };
+    };
+    services = {
+      paths = [ "/srv" ];
+      repo = "/mnt/Backup/services";
+      startAt = "*-*-* 2:00:00";
+      encryption = {
+        mode = "repokey";
+        passCommand = "cat /root/.borg-pass";
+        # passCommand = "${pkgs.pass}/bin/pass Misc/ServerBackup"; TODO
+      };
+      prune.keep = {
+        daily = 7;
+        weekly = 4;
+      };
     };
   };
 
-  systemd.services."backup" = {
-    script = ''
-      set -eu
-
-      # My Stuff
-      ${pkgs.borgbackup}/bin/borg create /mnt/Backup/home::$(date +%m-%d) /home/ben
-
-      # All the services in /srv
-      ${pkgs.borgbackup}/bin/borg create /mnt/Backup/services::$(date +%m-%d) /srv
-    '';
-    serviceConfig = {
-      EnvironmentFile = "/root/.secrets";
-      Type = "oneshot";
-    };
-  };
-
-  # Weekly backup pruning
-  systemd.timers."prune_backups" = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "Sun 3:00:00";
-      Unit = "prune_backups.service";
-    };
-  };
-
-  systemd.services."prune_backups" = {
-    script = ''
-      set -eu
-
-      ${pkgs.borgbackup}/bin/borg prune --keep-daily=7 --keep-weekly=4 /mnt/Backup/home
-      ${pkgs.borgbackup}/bin/borg prune --keep-daily=7 --keep-weekly=4 /mnt/Backup/services
-    '';
-    serviceConfig = {
-      EnvironmentFile = "/root/.secrets";
-      Type = "oneshot";
-    };
-  };
-
+  # TODO this can also be done with the nixos smart service
   # SMART scan
   systemd.timers."smart" = {
     wantedBy = [ "timers.target" ];
